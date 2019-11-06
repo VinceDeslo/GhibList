679338d484d04d9557ccd42ed6643b1e899c129d
VinceDeslo <v.desloover2@gmail.com>
2019-11-06 09:07:57 -0500

    Added login and register forms.

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
+import {Card, Logo, Form, Input, Button} from '../components/AuthForm';
+import {Link} from 'react-router-dom';
 
 export default class Login extends Component{
     
     render(){
         return(
-            <div>
-                <h1>Login page</h1>
-            </div>
+            <Card>
+                <Form>
+                    <Input type="email" placeholder="Email or username"/>
+                    <Input type="password" placeholder="Enter password"/>
+                    <Button>Login</Button>
+                </Form>
+                <Link to="/register">Or register here</Link>
+            </Card>
         )
     }
 }
--- src/components/register.component.js
+++ src/components/register.component.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
+import {Card, Logo, Form, Input, Button} from '../components/AuthForm';
+import {Link} from 'react-router-dom';
 
 export default class Register extends Component{
 
     render(){
         return(
-            <div>
-                <h1>Register page</h1>
-            </div>
+            <Card>
+                <Form>
+                    <Input type="email" placeholder="Email or username"/>
+                    <Input type="password" placeholder="Enter password"/>
+                    <Input type="password" placeholder="Enter password again"/>
+                    <Button>Register</Button>
+                </Form>
+                <Link to="/login">Or login here</Link>
+            </Card>
         )
     }
 }
